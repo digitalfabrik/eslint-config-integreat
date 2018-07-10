566b3c3aec42373d4553a7dea45c8735d0bafb2b
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-10 09:11:41 +0200

    Allow weak types

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -38,7 +38,7 @@ module.exports = {
     "flowtype/generic-spacing": [2, "never"],
     "flowtype/no-primitive-constructor-types": 2,
     "flowtype/no-types-missing-file-annotation": 2,
-    "flowtype/no-weak-types": 2,
+    "flowtype/no-weak-types": 1,
     "flowtype/object-type-delimiter": [2, "comma"],
     "flowtype/require-parameter-type": [
       2,
@@ -51,7 +51,8 @@ module.exports = {
       "always",
       {
         excludeArrowFunctions: true,
-        annotateUndefined: "never"
+        annotateUndefined: "never",
+        excludeMatching: ["^render$"]
       }
     ],
     "flowtype/require-valid-file-annotation": 2,
